7742fa4df3eb6d0f18206aa1a318baf40c90ef70
Marema6476 <marema.motaung@gmail.com>
2026-01-09 15:07:16 +0200

    Uncomment guessing game logic in GuessingGame.js

--- PROJECTS/Guessing game/GuessingGame.js
+++ PROJECTS/Guessing game/GuessingGame.js
@@ -1,20 +1,20 @@
 /*------with 1 prompt-------*/
-// // Random secret number between 1 and 20
-// var secret = Math.floor(Math.random() * 20) + 1;
+// Random secret number between 1 and 20
+var secret = Math.floor(Math.random() * 20) + 1;
 
-//  // Initialize guess with null so the while loop runs at least once
-// var guess = null;
+ // Initialize guess with null so the while loop runs at least once
+var guess = null;
 
-// while (guess !== secret) {
-//     guess = parseInt(prompt("Please guess the secret number (1-20)"));
+while (guess !== secret) {
+    guess = parseInt(prompt("Please guess the secret number (1-20)"));
 
-//     if (guess === secret) {
-//         alert("Correct Guess!");
-//     } else if (guess < secret) {
-//         alert("Incorrect, too low");
-//     } else if (guess > secret) {
-//         alert("Incorrect, too high");
-//     } else {
-//         alert("Invalid input. Please enter a number.");
-//     }
-// }
+    if (guess === secret) {
+        alert("Correct Guess!");
+    } else if (guess < secret) {
+        alert("Incorrect, too low");
+    } else if (guess > secret) {
+        alert("Incorrect, too high");
+    } else {
+        alert("Invalid input. Please enter a number.");
+    }
+}
